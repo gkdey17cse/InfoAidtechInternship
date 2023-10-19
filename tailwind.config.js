4ded0b66494a7a48f0a8118438758a07ceaa0eb9
gkdey17cse <gkdey17cse@gmail.com>
2023-10-19 12:14:07 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html",
+    "Task2_Calculator.html",
+    "Task1_ProductLandingPage.html",
+    "Task3_TemparatureConver.html",
+    "Task4_Resturant_Home.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
